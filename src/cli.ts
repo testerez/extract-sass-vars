f25a5b883b6d4f7c7a56803e91135830cee59eeb
Tom Esterez <testerez@gmail.com>
2017-06-23 14:52:00 -0400

    cli: support --include-path -v -h

--- src/cli.ts
+++ src/cli.ts
@@ -2,20 +2,39 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import extractScssVariables from './extractScssVariables';
+import * as _yargs from 'yargs';
+
+const yargs = _yargs
+  .usage('Usage: extract-sass-vars <file.scss> [args]')
+  .option('include-path', {
+    describe: 'Path to look for imported scss files. If many, join with commas.',
+  })
+  .option('v', {describe: 'verbose'})
+  .help('h')
+  .showHelpOnFail(true);
+const { argv } = yargs;
 
 try {
-  const filePath = process.argv[2];
+  const filePath = argv._[0];
   if (!filePath) {
-    throw 'Usage: extract-sass-vars <SCSS file path>';
+    yargs.showHelp();
+    throw '';
   }
   const fullFilePath = path.resolve(filePath);
-  console.log(JSON.stringify(
-    extractScssVariables(fullFilePath),
-    null,
-    2,
-  ));
+
+  const result = extractScssVariables(
+    fullFilePath,
+    {
+      includePaths: (argv.includePath || '')
+        .split(',')
+        .filter((s: string) => s)
+        .map((s: string) => path.resolve(s))
+    }
+  );
+
+  console.log(JSON.stringify(result, null, 2,));
 } catch (error) {
-  console.error(error.message || error);
+  console.error(argv.v ? error : error.message || error);
   process.exit(1); 
 }
 
