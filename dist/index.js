e490cf41297fcf3ef616189a3f0f22b512ccc522
Tom Esterez <testerez@gmail.com>
2017-06-23 14:55:59 -0400

    Bump version

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var extractScssVariables_1 = require("./extractScssVariables");
-exports.extractScssVariables = extractScssVariables_1.default;
